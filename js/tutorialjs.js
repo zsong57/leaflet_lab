f3a54a6c4e04602127d52011b11f9e7cf07a686a
zsong57 <zsong57@wisc.edu>
2016-02-10 14:58:49 -0600

    start tutorial

---
+++ js/tutorialjs.js
@@ -0,0 +1,28 @@
+/* Example from Leaflet Quick Start Guide*/
+//  initialize the map and set its view to our chosen geographical coordinates and a zoom level
+var map = L.map('map').setView([51.505, -0.09], 13);
+// add base map with tile layers from website
+L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(map);
+
+// add a marker
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+// add a cirle
+var circle = L.circle([51.508, -0.11], 500, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(map);
+
+// add a polygon
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
